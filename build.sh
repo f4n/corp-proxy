95d8bea52e1d000e1281768515ffd8a0c3953358
Fabian Keller <mail@fabiankeller.de>
2017-01-24 18:22:52 +0100

    Added proxy settings for build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+BUILD_ARGS=""
+if [ -n "$1" ]
+then
+  BUILD_ARGS="--build-arg http_proxy=$1 --build-arg https_proxy=$1 --build-arg HTTP_PROXY=$1 --build-arg HTTPS_PROXY=$1"
+fi
+
 #create corp-proxy docker image
-docker build -f Dockerfile --rm -t corp-proxy .
+docker build -f Dockerfile --rm -t corp-proxy $BUILD_ARGS .
