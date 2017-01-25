b14c5c60720f4ae8f2dacfd7dc05470aa5446e7f
f4n <f4n@users.noreply.github.com>
2017-01-25 10:51:27 +0100

    Run squid as user

--- files/start-squid.sh
+++ files/start-squid.sh
@@ -2,20 +2,26 @@
 
 set -e
 
+#adduser -D -u $USER_ID -g 1000 $USER_NAME #does not work? uid is use?!?
+echo "$USER_NAME:x:$USER_ID:1000:nobody:/:/bin/sh" > /etc/passwd
+
 CHOWN=$(/usr/bin/which chown)
+CHMOD=$(/usr/bin/which chmod)
 SQUID=$(/usr/bin/which squid)
 
 # Ensure permissions are set correctly on the Squid cache + log dir.
-"$CHOWN" -R squid:squid /var/cache/squid
-"$CHOWN" -R squid:squid /var/log/squid
+"$CHOWN" -R $USER_NAME:$USERNAME /var/cache/squid
+"$CHOWN" -R $USER_NAME:$USERNAME /var/log/squid
+"$CHMOD" 777 /var/run/
 
 # Prepare the cache using Squid.
 echo "Initializing cache..."
-"$SQUID" -z
+/bin/su -c "$SQUID -z" $USER_NAME
 
 # Give the Squid cache some time to rebuild.
 sleep 5
 
 # Launch squid
 echo "Starting Squid..."
-exec "$SQUID" -NYCd 1
+/bin/su -c "$SQUID -NYCd 1" $USER_NAME
+#/bin/sh
