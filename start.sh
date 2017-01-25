c411868a9abcfeab72a74156922969573bc053c6
f4n <f4n@users.noreply.github.com>
2017-01-25 10:51:55 +0100

    Set local user as squid user

--- start.sh
+++ start.sh
@@ -21,4 +21,6 @@ docker run  -d  \
             -v $SCRIPT_DIR/cache:/var/cache/squid:rw \
             -v $SCRIPT_DIR/log:/var/log/squid:rw \
             -p 127.0.0.1:3128:3128 \
+            -e USER_NAME=`whoami` \
+            -e USER_ID=`id -u` \
             corp-proxy:latest
